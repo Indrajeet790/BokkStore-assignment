a99e13973a9c2866cd7e9fb725d3909787bed226
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-19 15:30:49 +0530

    create  a server for bookstore using express

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log("server is not running on port");
+  } else {
+    console.log(`server running on port ${PORT}`);
+  }
+});
