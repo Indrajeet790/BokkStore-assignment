3dcc11c5f222cb8b5304484ae6bdcbd5ad30790c
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-19 16:36:16 +0530

    require router into main file index.js and also add middleware for  parsing data from body

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const PORT = 8000;
 // database connection
 const DbConnection = require("./config/DbConnection");
 
+// use middleware for parsing data from body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//require router
+const itemRoutes = require("./Router/itemRoutes");
+
+// Routes
+app.use("/api/items", itemRoutes);
+
 app.listen(PORT, (err) => {
   if (err) {
     console.log("server is not running on port");
