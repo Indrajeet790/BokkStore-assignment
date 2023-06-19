95c8553e98668ae82653ebd1c55276ba64309320
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-19 15:56:08 +0530

    require database connection file into index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
+// database connection
+const DbConnection = require("./config/DbConnection");
+
 app.listen(PORT, (err) => {
   if (err) {
     console.log("server is not running on port");
