6b06ac0ae75c922f0b44566a1351fa50f369c2de
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-19 16:04:46 +0530

    create a  ItemSchema for bookStore with BookName, author, and Price

--- models/BooksModel.js
+++ models/BooksModel.js
@@ -1,23 +1,20 @@
 const mongoose = require("mongoose");
 
-const bookSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    author: {
-      type: String,
-      required: true,
-    },
-    price: {
-      type: Number,
-      required: true,
-    },
+const itemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
   },
-  { timestamps: true }
-);
+  author: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
 
-const Book = mongoose.model("Books", bookSchema);
+const Item = mongoose.model("Item", itemSchema);
 
-module.exports = Book;
+module.exports = Item;
