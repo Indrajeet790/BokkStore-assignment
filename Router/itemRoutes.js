2ac72783b33fc5d5253517bf70b3bdf1c3b91a36
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-19 17:05:35 +0530

    create route for retrieve item by its ID.

--- Router/itemRoutes.js
+++ Router/itemRoutes.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { createItem, getAllItems } = require("../controller/booksController");
+const {
+  createItem,
+  getAllItems,
+  getItemById,
+} = require("../controller/booksController");
 
 // Post route for create a new book item
 router.post("/", createItem);
 
 // create route for get all item from database.
 router.get("/", getAllItems);
+
+//Retrieve a specific item by its ID.
+router.get("/:id", getItemById);
 
 module.exports = router;
