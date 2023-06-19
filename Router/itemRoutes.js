a9e5e6cc8275442e49cd277e0e16f33331ea57d3
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-19 16:45:58 +0530

    create routes for retrieve all items

--- Router/itemRoutes.js
+++ Router/itemRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createItem } = require("../controller/booksController");
+const { createItem, getAllItems } = require("../controller/booksController");
 
 // Post route for create a new book item
 router.post("/", createItem);
+
+// create route for get all item from database.
+router.get("/", getAllItems);
 
 module.exports = router;
