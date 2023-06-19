aa1ec555813223cc39852a0564f00b07e5a48ae3
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-19 16:34:04 +0530

    fixing error

--- controller/booksController.js
+++ controller/booksController.js
@@ -1,1 +1,21 @@
-const { Item } = require("../models/BooksModel");
+const Item = require("../models/BooksModel");
+
+//- Create a new item in the database.
+const createItem = async (req, res) => {
+  try {
+    const data = await Item.create(req.body);
+    if (data) {
+      return res.status(201).json(data);
+    }
+
+    return res.status(400).json({
+      error: "item not created",
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  createItem,
+};
