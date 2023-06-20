1dea5c2b9ce043c07f2f79bed3541fc8f22bfecd
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-20 13:36:01 +0530

    import logger file into database connection file

--- config/DbConnection.js
+++ config/DbConnection.js
@@ -1,4 +1,9 @@
+// require mongoose
 const mongoose = require("mongoose");
+
+// import logger.js
+const logger = require("../utils/logger");
+// mongoose connection
 mongoose.connect("mongodb://127.0.0.1:27017/BookStore").then(() => {
-  console.log("database successfully connected");
+  logger.BookLogger.info("database successfully connected");
 });
